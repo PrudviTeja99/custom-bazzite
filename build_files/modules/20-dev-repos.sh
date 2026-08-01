eb708c0c623179fb7da9ca59e5e2dfcef23f7bfb
PrudviTeja99 <prudvitejabandam987@gmail.com>
2026-08-01 12:13:08 +0530

    fix(build): make /opt a standard directory for Brave/VS Code RPM unpacking

--- build_files/modules/20-dev-repos.sh
+++ build_files/modules/20-dev-repos.sh
@@ -8,4 +8,11 @@ set -ouex pipefail
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # 2. Add VS Code repository
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+cat <<EOF > /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF

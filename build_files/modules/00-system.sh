8673470adea0c94773e2a0c96785f8b8f1e7fb23
PrudviTeja <65024971+PrudviTeja99@users.noreply.github.com>
2026-08-01 23:18:57 +0530

    Update 00-system.sh

--- build_files/modules/00-system.sh
+++ build_files/modules/00-system.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 # Place systemd unit activations, udev rules, or system tweaks here
 
 # Add rpm fusion repo
-dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Enable Podman socket
 systemctl enable podman.socket
